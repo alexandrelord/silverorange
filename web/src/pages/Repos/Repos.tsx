ecd9af06bab1013120a93ebd05049eb9d20c903c
Alexandre Lord <99230887+alexandrelord@users.noreply.github.com>
2022-12-05 17:00:44 -0500

    render table with basic info; add click envent to fetch latest commit from repo; add buttons to filter repo by language

--- web/src/pages/Repos/Repos.tsx
+++ web/src/pages/Repos/Repos.tsx
@@ -1,8 +1,10 @@
 import { useState, useEffect } from 'react';
 import { IRepo } from '../../types/Repos';
+// import styles from './Repos.module.css';
 
 export function Repos() {
   const [repos, setRepos] = useState<IRepo[]>([]);
+  const [language, setLanguage] = useState('All');
   const [loading, setLoading] = useState(true);
 
   useEffect(() => {
@@ -25,20 +27,94 @@ export function Repos() {
     fetchRepos();
   }, []);
 
+  async function handleClick(url: string) {
+    const commitsUrl = url.replace('{/sha}', '');
+
+    const fetchCommits = async () => {
+      const response = await fetch(commitsUrl);
+      if (response.ok) {
+        const result = await response.json();
+        const author = result[0].commit.author.name;
+        const message = result[0].commit.message;
+        const date = result[0].commit.author.date;
+        console.log(author, message, date);
+      }
+    };
+    fetchCommits();
+  }
+
+  function renderReposTable() {
+    return (
+      <table>
+        <thead>
+          <tr>
+            <th>Name</th>
+            <th>Description</th>
+            <th>Language</th>
+            <th>Forks Count</th>
+          </tr>
+        </thead>
+        <tbody>
+          {language === 'All' &&
+            repos.map((repo) => (
+              <tr key={repo.id} onClick={() => handleClick(repo.commits_url)}>
+                <td>{repo.name}</td>
+                <td>{repo.description}</td>
+                <td>{repo.language}</td>
+                <td>{repo.forks_count}</td>
+              </tr>
+            ))}
+          {language &&
+            repos
+              .filter((repo) => repo.language === language)
+              .map((repo) => (
+                <tr key={repo.id} onClick={() => handleClick(repo.commits_url)}>
+                  <td>{repo.name}</td>
+                  <td>{repo.description}</td>
+                  <td>{repo.language}</td>
+                  <td>{repo.forks_count}</td>
+                </tr>
+              ))}
+        </tbody>
+      </table>
+    );
+  }
+
+  function renderLanguageButtons() {
+    const languages: string[] = ['All'];
+    repos.forEach((repo) => {
+      if (!languages.includes(repo.language)) {
+        languages.push(repo.language);
+      }
+    });
+
+    return (
+      <div>
+        {languages.map((lang, id) => (
+          <button
+            key={id}
+            onClick={() => {
+              setLanguage(lang);
+              renderReposTable();
+            }}
+          >
+            {lang}
+          </button>
+        ))}
+      </div>
+    );
+  }
+
   return (
-    <div>
+    <section>
       <h1>Repos</h1>
       {loading ? (
         <p>Loading...</p>
       ) : (
-        <ul>
-          {repos.map((repo) => (
-            <li key={repo.id}>
-              <a href={repo.html_url}>{repo.name}</a>
-            </li>
-          ))}
-        </ul>
+        <>
+          {renderReposTable()} {renderLanguageButtons()}
+        </>
       )}
-    </div>
+    </section>
   );
 }
