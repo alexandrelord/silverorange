5f2a2135e7532c0e0dcace133d86f4bded63d83b
Alexandre Lord <99230887+alexandrelord@users.noreply.github.com>
2022-12-05 16:46:17 -0500

    fetch repos from api and display basic list

--- web/src/pages/Repos/Repos.tsx
+++ web/src/pages/Repos/Repos.tsx
@@ -1,3 +1,44 @@
+import { useState, useEffect } from 'react';
+import { IRepo } from '../../types/Repos';
+
 export function Repos() {
-  return <div>Repos</div>;
+  const [repos, setRepos] = useState<IRepo[]>([]);
+  const [loading, setLoading] = useState(true);
+
+  useEffect(() => {
+    const fetchRepos = async () => {
+      const response = await fetch('http://localhost:4000/repos');
+      if (response.ok) {
+        const result = await response.json();
+        setRepos(
+          result.data.sort(function (a: IRepo, b: IRepo) {
+            return (
+              // sort in reverse chronological order by creation date
+              new Date(b.created_at).getTime() -
+              new Date(a.created_at).getTime()
+            );
+          })
+        );
+      }
+      setLoading(false);
+    };
+    fetchRepos();
+  }, []);
+
+  return (
+    <div>
+      <h1>Repos</h1>
+      {loading ? (
+        <p>Loading...</p>
+      ) : (
+        <ul>
+          {repos.map((repo) => (
+            <li key={repo.id}>
+              <a href={repo.html_url}>{repo.name}</a>
+            </li>
+          ))}
+        </ul>
+      )}
+    </div>
+  );
 }
